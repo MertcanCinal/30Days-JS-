12aac5806f626eb6c0b528a7225dd4f5549fed3d
MertcanCinal <54298411+MertcanCinal@users.noreply.github.com>
2023-03-23 18:34:43 +0300

    Add files via upload

---
+++ 30.days.JS/30.days.js
@@ -0,0 +1,29 @@
+// bir satırlık açıklama yapabilirsin!
+console.log('Hello World')
+/* istediğin
+kadar
+açıklama
+yapabilirsin! */
+
+console.log(2 + 3)//toplama
+console.log(3 - 2)//çıkartma
+console.log(2 * 3)//çarpma
+console.log(3 / 2)//bölme
+console.log(3 % 2)//bölümünden kalan
+console.log(3 ** 2)//üs alma
+
+console.log(typeof 'Yaratıcı')//string ifade
+console.log(typeof 33)//number
+console.log(typeof true)//evet ya da hayır
+console.log(typeof null)//obje
+console.log(typeof undefined)//undefined
+
+/*Variable
+değişkenler numara ile başlayamaz!
+$ ve _ hariç özel karakterler kullanılamaz!*/
+
+const//değişmeyecek veriler için kullanılır!(ülkeler,matematik..)
+const PI = 3.14
+
+let//değiştirilebilir veriler için kullanılır!
+let name = 'MC'
